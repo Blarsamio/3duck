9f8f485c3418e7a748e8da9d25bdf1e4da2580a7
Patricio <patriciopherrero@gmail.com>
2024-09-20 12:00:31 +0200

    master

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,10 +3,12 @@ import React, {Suspense} from 'react'
 
 const Hero = () => {
     return (
-        <section className='min-h-screen w-full flex flex-col relative bg-[#a7c6da]'>
-            <div className='w-full flex flex-col sm:nt-36 mt-96 c-space gap-3'>
-                <h1 className='lg:text-9xl text-8xl font-medium text-white text-center font-satoshiBlack'>HI, I AM PATRICIO <span className='waving-hand'></span></h1>
-                <p className='text-white hero_tag'>I like bringing life into code, <br/> and code into life.</p>
+        <section className='min-h-screen w-full flex flex-col relative' id='home'>
+            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+                <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+                    Hi, I am Patricio <span className="waving-hand">👋</span>
+                </p>
+                <p className="hero_tag text-gray_gradient">Bringing life into code, <br/>and code into life.</p>
             </div>
         </section>
     )
